abbf53e6c68d1b981bfac5651a10e5584ed3e19d
AlejandroGarJ <alejgarciaj@gmail.com>
2024-03-21 12:14:59 +0100

    Quite cosas de ruben

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -86,9 +86,11 @@ class Usuario extends Model
 		//Usuario con el correo que proporciona el usuario
 		$usuario = Usuario::where('correo', $data['correo'])->first();
 		if ($usuario == null) {
+			//Si el correo no existe, se añade correo como error a la respuesta
 			array_push($respuesta, "correo");
 		} else {
 
+			//Si la contraseña no corresponde, se añade el error a la respuesta
 			if (password_verify($data['contrasena'], $usuario->contrasena) == false)
 				array_push($respuesta, "contrasena");
 		}
@@ -96,77 +98,7 @@ class Usuario extends Model
 			array_push($respuesta, $usuario->dni);
 		}
 		return $respuesta;
-	}
-
-	public static function obtenerClubes($data)
-	{
-		$dni = $data;
-
-		$clubs = Usuarioclub::where('dni', $dni)->get();
-
-		$clubsUsuario = Club::whereIn('id_club', $clubs->pluck('id_club'))->get();
-
-		return $clubsUsuario;
-	}
-
-	public static function crearClub($data)
-	{
-		$respuesta = array();
-		$club = new Club();
-		$club->nombre = $data['nombre'];
-		$club->usuarioClub = $data['usuarioClub'];
-		$club->codigoAcceso = password_hash($data['codigoAcceso'], PASSWORD_DEFAULT);
-
-		try {
-			$club->save();
-			$usuarioClub = new Usuarioclub();
-			$usuarioClub->dni = $data['DNI'];
-			$usuarioClub->id_club = $club->id_club;
-			$usuarioClub->save();
-		} catch (QueryException $e) {
-			if ($e->getCode() === '23000') {
-				array_push($respuesta, "Ya existe un club con ese nombre");
-			}
-		}
-		return $respuesta;
-
-	}
-
-	public static function unirseAClub($data)
-	{
-		$respuesta = array();
-
-		$club = Club::where('usuarioClub', $data['usuarioClub'])->first();
-		if ($club == null) {
-			array_push($respuesta, "club");
-		} else {
-			$usuarioClub = Usuarioclub::where('id_club', $club->id_club)
-				->where('dni', $data['DNI'])
-				->first();
-			if ($usuarioClub != null) {
-				array_push($respuesta, "club2");
-			} else {
-				if (password_verify($data['codigoAcceso'], $club->codigoAcceso) == false)
-					array_push($respuesta, "codigoAcceso");
-			}
-
-		}
-		if (count($respuesta) == 0) {
-			$usuarioClub2 = new Usuarioclub();
-			$usuarioClub2->dni = $data['DNI'];
-			$usuarioClub2->id_club = $club->id_club;
-			$usuarioClub2->save();
-
-		}
-		return $respuesta;
-	}
-
-	public static function dejarClub($data)
-	{
-		$club = Usuarioclub::where('dni', $data['DNI'])
-			->where('id_club', $data['id_club'])
-			->delete();
-		return true;
+		//Si se devuelve un array vacio esque inicio correctamente
 	}
 
 	public function cuantosUsuarios()
